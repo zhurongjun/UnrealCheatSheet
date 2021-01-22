48cd141fb3039993966eeaac6143105864ec2dfc
unknown <huai1115@126.com>
2021-01-22 18:22:15 +0800

    New Slate : you can show the detail panel anywhere now!

---
+++ Plugins/MiscTools/Source/MiscToolsEditor/Private/UI/SAutoSettings.cpp
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/SAutoSettings.h"
+
+#include "SlateOptMacros.h"
+#include "Widgets/Layout/SScrollBox.h"
+
+BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+
+END_SLATE_FUNCTION_BUILD_OPTIMIZATION
+
+void SAutoSettings::Construct(const FArguments& InArgs, UObject* SettingObject)
+{
+	FPropertyEditorModule& PropertyEditorModule = FModuleManager::LoadModuleChecked<FPropertyEditorModule>(
+        "PropertyEditor");
+
+	FDetailsViewArgs DetailsViewArgs(
+        /*bUpdateFromSelection*/ false,
+                                 /*bLockable*/ false,
+                                 /*bAllowSearch*/ false,
+                                 /*InNameAreaSettings*/ FDetailsViewArgs::HideNameArea,
+                                 /*bHideSelectionTip*/ true,
+                                 /*InNotifyHook*/ nullptr,
+                                 /*InSearchInitialKeyFocus*/ false,
+                                 /*InViewIdentifier*/ NAME_None);
+
+	DetailsViewArgs.DefaultsOnlyVisibility = EEditDefaultsOnlyNodeVisibility::Automatic;
+	DetailsViewArgs.bShowOptions = false;
+	DetailsViewArgs.bAllowMultipleTopLevelObjects = true;
+
+
+	DetailsPanel = PropertyEditorModule.CreateDetailView(DetailsViewArgs);
+
+	if (SettingObject)
+	{
+		UISettings = SettingObject;
+		DetailsPanel->SetObject(UISettings, true);
+	}
+
+	ChildSlot
+    [
+        SNew(SScrollBox)
+        + SScrollBox::Slot()
+        .Padding(0.0f)
+        [
+            SNew(SVerticalBox)
+            + SVerticalBox::Slot()
+              .AutoHeight()
+              .Padding(0)
+            [
+                DetailsPanel.ToSharedRef()
+            ]
+        ]
+    ];
+}
